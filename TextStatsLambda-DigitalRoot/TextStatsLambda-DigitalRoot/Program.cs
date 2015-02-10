d183bb417717f7bf0e28b27d2a89594164d4c28b
Sergio Alifano <sergio.alifano@gmail.com>
2015-02-10 13:55:16 -0700

    Digital root with Lambda Expression

--- TextStatsLambda-DigitalRoot/TextStatsLambda-DigitalRoot/Program.cs
+++ TextStatsLambda-DigitalRoot/TextStatsLambda-DigitalRoot/Program.cs
@@ -10,43 +10,77 @@ namespace TextStatsLambda_DigitalRoot
     {
         static void Main(string[] args)
         {
-
         }
 
         public static int DigitalRoot(string rootThisNumber)
         {
-            return 0;
+            int number;
+            int sum = 0;
+
+            //LAMBDA EXPRESSION
+        /*    while (rootThisNumber.Length > 1)
+            {
+                sum = rootThisNumber.Sum(x => int.Parse(x.ToString()));
+                rootThisNumber = sum.ToString();
+            }
+            return sum;
+        */
+
+            //check if it's a valid input
+            if (int.TryParse(rootThisNumber, out number))
+            {
+                //loop until it's a single digit
+                while (rootThisNumber.Length > 1)
+                {
+                    //loop through the string 
+                    foreach (char digit in rootThisNumber)
+                    {
+                        //sum the digits of the number
+                        sum += int.Parse(digit.ToString());
+                    }
+                    
+                    rootThisNumber = sum.ToString();
+                    sum = 0;
+                }
+            }
+
+            return int.Parse(rootThisNumber);
         }
 
         public static int NumberOfWords(string inputString)
         {
-            return 0;
+
+            return inputString.Split().Count();
         }
 
         public static int NumberOfVowels(string inputString)
         {
-            return 0;
+
+            return inputString.Count(x => "aeiou".Contains(char.ToLower(x)));
         }
 
         public static int NumberOfConsonants(string inputString)
         {
-            return 0;
+
+           return inputString.Count(x => "bcdfghjklmnpqrstvwxyz".Contains(char.ToLower(x)));
+           
         }
 
         public static int NumberOfSpecialCharacters(string inputString)
         {
             // .,?;'!@#$%^&*() and spaces are considered special characters
-            return 0;
+            return inputString.Count(x => ".,?;'!@#$%^&*() ".Contains(char.ToLower(x)));
         }
 
         public static string LongestWord(string inputString)
         {
-            return string.Empty;
+
+            return inputString.Split().OrderBy(x => x.Length).Last();
         }
 
         public static string ShortestWord(string inputString)
         {
-            return string.Empty;
+            return inputString.Split().OrderBy(x => x.Length).First();
         }
 
 
